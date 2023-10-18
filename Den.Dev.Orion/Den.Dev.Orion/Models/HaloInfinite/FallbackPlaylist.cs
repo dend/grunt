63c21810d04c1f6ca6c4441aaae7a3004db19f5d
Den <1389609+dend@users.noreply.github.com>
2023-10-18 14:14:53 -0700

    Update with missing files

---
+++ Den.Dev.Orion/Den.Dev.Orion/Models/HaloInfinite/FallbackPlaylist.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="FallbackPlaylist.cs" company="Den Delimarsky">
+// Developed by Den Delimarsky.
+// Den Delimarsky licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+// The underlying API powering Den.Dev.Orion is managed by 343 Industries and Microsoft. This wrapper is not endorsed by 343 Industries or Microsoft.
+// </copyright>
+
+namespace Den.Dev.Orion.Models.HaloInfinite
+{
+    /// <summary>
+    /// Container class for a fallback playlist.
+    /// </summary>
+    public class FallbackPlaylist
+    {
+        /// <summary>
+        /// Gets or sets the fallback playlist ID.
+        /// </summary>
+        public string? FallbackPlaylistId { get; set; }
+    }
+}
